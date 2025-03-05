9457eab6d97cd45721343fd616ed98efa5ece304
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-06 01:22:30 +0200

    reworked navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,15 +1,35 @@
-export default function Navbar() {
+import { ArrowUpRight, Earth,Menu } from "lucide-react";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const NavbarItem = ({ label, hasIcon, href }) => (
+    <li className="hover:text-[#0BA159]">
+      <a href={href}>{label}</a>
+      {hasIcon && <ArrowUpRight className="inline stroke-3 size-4.5 ml-2" />}
+    </li>
+  );
   return (
-    <nav className="flex md:flex-row lg:flex-row py-5 px-12 font-bold text-xl tracking-wider text-black items-center justify-between">
-      <div>  
-        <a>NatureTek Academy</a>
+    <nav className="flex p-8 flex-row gap-8 items-center justify-between md:justify-start">
+      <div className="flex flex-row gap-2 items-center">
+        <Earth color="#0BA159" size={36} />
+        <a className="font-bold text-xl">NatureTek Academy</a>
       </div>
-      <div>
-        <ul className="flex justify-end gap-20">
-          <li><a>Learning Areas</a></li>
-          <li><a>Contact Us</a></li>
+      <div className="hidden md:block">
+        <ul className="flex flex-row gap-8 font-bold text-lg text-[#63677A]">
+          <NavbarItem label="Home" hasIcon={false} href="#home" />
+          <NavbarItem label="Programs" hasIcon={false} href="#Programs" />
+          <NavbarItem label="Contact" hasIcon={true} href="#Contact" />
+          <NavbarItem label="Roadmap" hasIcon={true} href="#Roadmap" />
         </ul>
+      </div>
+      <div className="md:hidden items-center">
+        <button className="align-bottom" onClick={() => setIsOpen(!isOpen)}>
+          <Menu size={24} />
+        </button>
+        {isOpen && <></>}
       </div>
     </nav>
   );
-}
+};
+export default Navbar;
