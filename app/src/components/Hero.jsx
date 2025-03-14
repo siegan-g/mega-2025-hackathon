61bd3632886ba7e74f69491aa0da62028aa1b955
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-14 02:56:43 +0200

    added button colour to component

--- app/src/components/Hero.jsx
+++ app/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
-  const HeroButton = ({ label, href }) => (
+  const HeroButton = ({ label, colour,href }) => (
     <div className="flex justify-between">
-      <button className="display-block bg-[#0BA159] font-bold text-white px-4 py-2 w-full rounded-md text-xl md:text-2xl">
+      <button className={`display-block bg-[${colour}] font-bold text-white px-4 py-2 w-full rounded-md text-xl md:text-2xl`}>
         <a className="px-5 py-12" href={href}>
           {label}
         </a>
@@ -21,18 +21,17 @@ const Hero = () => {
           </div>
           <div className="text-md md:text-lg px-10 md:px-0">
             <h3 className="text-[#63677A]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
-              bibendum, eros ut rutrum euismod, nunc justo semper justo, in
-              hendrerit metus mi at ante. Etiam a orci a felis euismod faucibus
-              in vitae turpis.
+              Together, we can build a future where people, planet, and
+              prosperity thrive in harmony. Join us in driving awareness and
+              action for the United Nations&apos; Sustainable Development Goals.
             </h3>
           </div>
           <div className="flex flex-col md:flex-row gap-12">
-            <HeroButton label="Play" href="#programs" />
+            <HeroButton label="Play" href="#programs" colour="#0BA159" />
             <HeroButton
-              label="Learn More"
+              label="Learn"
               href="#learn"
-              className="bg-[#63677A]"
+              colour="#63677A"
             />
           </div>
         </div>
