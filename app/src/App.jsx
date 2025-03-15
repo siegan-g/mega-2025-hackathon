9f832514e24f71b8587143da2b605bcdbb8a4b57
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-15 18:19:57 +0200

    fixed incorrect imports

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,5 +1,4 @@
-import { ExpandableCard } from "./components/ExpandableCard";
-import Fundraisers from "./components/Fundraisers";
+import ExpandableCard from "./components/ExpandableCard";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -8,7 +7,6 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      <ExpandableCard/>
     </>
   );
 }
