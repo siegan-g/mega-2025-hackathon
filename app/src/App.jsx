b93dd0cdce1f784f6c6b4e46277747c367b7ca5a
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-16 01:32:12 +0200

    added Ngo section

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Ngo from "./components/Ngo";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Ngo />
     </>
   );
 }
