fc350dd863e7736f37cada84e99ec20d9b389169
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-17 01:57:04 +0200

    fixed textarea spelling

--- app/src/components/Input.jsx
+++ app/src/components/Input.jsx
@@ -35,7 +35,7 @@ const Input = React.forwardRef(
         className="p-[2px] rounded-lg transition duration-300 group/input"
       >
         {textArea ? (
-          <textArea
+          <textarea
             type={type}
             className={cn(
               `flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
