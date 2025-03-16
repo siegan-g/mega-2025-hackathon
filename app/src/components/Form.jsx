2182cb9a166c7276d557093452d746a8f164dd0d
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-17 01:57:23 +0200

    Added server action for form submission

--- app/src/components/Form.jsx
+++ app/src/components/Form.jsx
@@ -1,16 +1,22 @@
 "use client";
-import React from "react";
+import React, { useActionState } from "react";
 import { Label } from "./Label";
 import { Input } from "./Input";
 import { cn } from "../lib/utils";
 import { Github } from "lucide-react";
 import { Header } from "./Header";
+import { submitForm } from "../actions/submit-form";
 
 export default function Form() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted");
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+    returnMessage: "",
+    errors: {},
   };
+  const [state, formAction] = useActionState(submitForm, initialState);
   return (
     <section id="contact" className="pt-30 px-12">
       <Header
@@ -18,35 +24,59 @@ export default function Form() {
         subtitle="Want to know more about our platform? Did something break? We're here to help!"
       />
       <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:px-8 shadow-input bg-white dark:bg-black">
-        <form className="my-8" onSubmit={handleSubmit}>
+        <form className="my-8" action={formAction}>
           <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
             <LabelInputContainer>
               <Label htmlFor="firstname">First name</Label>
-              <Input id="firstname" placeholder="John" type="text" />
+              <Input
+                id="firstname"
+                name="firstName"
+                placeholder="John"
+                defaultValue={state.firstName}
+                type="text"
+              />
             </LabelInputContainer>
             <LabelInputContainer>
               <Label htmlFor="lastname">Last name</Label>
-              <Input id="lastname" placeholder="Doe" type="text" />
+              <Input
+                id="lastname"
+                name="lastName"
+                placeholder="Doe"
+                defaultValue={state.lastName}
+                type="text"
+              />
             </LabelInputContainer>
           </div>
           <LabelInputContainer className="mb-4">
             <Label htmlFor="email">Email Address</Label>
-            <Input id="email" placeholder="johndoe@hotmail.com" type="email" />
+            <Input
+              id="email"
+              name="email"
+              placeholder="johndoe@hotmail.com"
+              defaultValue={state.email}
+              type="email"
+            />
           </LabelInputContainer>
           <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">Message</Label>
-            <Input id="email" placeholder="Send Message" type="text" textArea={true} className="h-20" />
-          </LabelInputContainer>
+            <Label htmlFor="message">Message</Label>
+            <Input
+              id="message"
+              name="message"
+              placeholder="Send Message"
+              defaultValue={state.message}
+              type="text"
+              textArea={true}
+              className="h-20"
+            />
+          </LabelInputContainer>{" "}
           <button
             className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset] cursor-pointer"
             type="submit"
           >
-            Send Message &rarr;
+            {state.returnMessage? state.returnMessage:"Send Message"}
             <BottomGradient />
           </button>
-
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
-
           <div className="flex flex-col space-y-4">
             <button
               className=" relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)] cursor-pointer"
