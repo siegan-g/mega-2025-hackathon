5563093445ddd65b597a5f6fba72cac13b88b481
tahliahouston <talzileigh@gmail.com>
2025-03-16 22:32:26 +0200

    changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,29 +3,15 @@ import { Features } from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Ngo from "./components/Ngo";
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Games from './components/Games';
-import WordleGame from './components/Wordle';
-import SnakeGame from './components/SnakeGame';
-import TriviaGame from './components/TriviaGame';
-import LearningHub from './components/LearningHub';
-import SDGChatbot from './components/SDGChatbot';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Analytics from './components/Analytics';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<><Navbar /><Hero /><Games /><LearningHub /><Analytics /><Features/>
-          <Ngo /></>} />
-        <Route path="/wordle" element={<WordleGame />} />
-        <Route path="/snake" element={<SnakeGame />} />
-        <Route path="/trivia" element={<TriviaGame />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+      <Hero />
+      <Features/>
+      <Ngo />
+    </>
   );
 }
 
