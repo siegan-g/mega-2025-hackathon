b6c2cb009e70b51d267348ce959fbde205e8edd1
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-16 02:04:33 +0200

    Added FeatureSection Component
    This one is a little different because it wasn't created to be as reusable. Many parts are hardcoded. So its a section on its own.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,12 @@
 import ExpandableCard from "./components/ExpandableCard";
+import { Games } from "./components/Games";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Hero />
+      <Games />
     </>
   );
 }
