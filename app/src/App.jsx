c722b32ae91b7b6c5cc07510e95bfc74dc9bf612
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-17 02:37:11 +0200

    added form component to main page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,29 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Features } from "./components/Features";
 
-import Analytics from './components/Analytics';
+import Analytics from "./components/Analytics";
 import Hero from "./components/Hero";
 import Ngo from "./components/Ngo";
-import Layout from './components/Layout';
-import Games from './components/Games';
-import WordleGame from './components/Wordle';
-import SnakeGame from './components/SnakeGame';
-import TriviaGame from './components/TriviaGame';
-import LearningHub from './components/LearningHub';
+import Layout from "./components/Layout";
+import Games from "./components/Games";
+import WordleGame from "./components/Wordle";
+import SnakeGame from "./components/SnakeGame";
+import TriviaGame from "./components/TriviaGame";
+import LearningHub from "./components/LearningHub";
+import Form from "./components/Form";
 
 function App() {
   return (
     <Router>
-      <Layout >
-      <Routes>
-        <Route path="/" element={<><Hero /><Features /><Ngo /></>} />
-        <Route path="/games" element={<Games />} />
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/learn" element={<LearningHub />} />
-        <Route path="/wordle" element={<WordleGame />} />
-        <Route path="/snake" element={<SnakeGame />} />
-        <Route path="/trivia" element={<TriviaGame />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Features />
+                <Ngo />
+                <Form />
+              </>
+            }
+          />
+          <Route path="/games" element={<Games />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/learn" element={<LearningHub />} />
+          <Route path="/wordle" element={<WordleGame />} />
+          <Route path="/snake" element={<SnakeGame />} />
+          <Route path="/trivia" element={<TriviaGame />} />
+        </Routes>
       </Layout>
     </Router>
   );
