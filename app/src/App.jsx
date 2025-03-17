180b12ae487df8a539f6b3a51d1c429b663ce78e
siegan-g <84604078+siegan-g@users.noreply.github.com>
2025-03-17 02:24:10 +0200

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,14 +3,16 @@ import { Features } from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Ngo from "./components/Ngo";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Features/>
+      <Features />
       <Ngo />
+      <Form />
     </>
   );
 }
